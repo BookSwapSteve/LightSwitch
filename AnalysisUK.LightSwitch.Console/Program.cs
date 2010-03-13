52bc20a8db09645656f8ee7d98d6db450650ab4c
Stephen.Harrison@AnalysisUK.com <Stephen.Harrison@AnalysisUK.com>
2010-03-13 23:54:17 +0000

    Added basics of project structure.

---
+++ AnalysisUK.LightSwitch.Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.SPOT;
+using DeviceSolutions.SPOT.Hardware;
+using Microsoft.SPOT.Hardware;
+using System.Threading;
+
+namespace AnalysisUK.LightSwitch.Console
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            OutputPort led = new OutputPort(MeridianP.Pins.LED, true);
+            InputPort button = new InputPort(MeridianP.Pins.SW1, true, Port.ResistorMode.PullUp);
+
+            while (true)
+            {
+                if (button.Read() == false)
+                {
+                    led.Write(false);
+                    Thread.Sleep(200);
+                    led.Write(true); 
+                    Thread.Sleep(200);
+                }
+            } 
+        }
+
+    }
+}
